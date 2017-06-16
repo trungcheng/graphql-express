9d09250dd3468d7accc57ac753a9504c5eb711c0
trungcheng <trungs1bmt@gmail.com>
2017-06-16 14:03:34 +0700

    initial

--- server.js
+++ server.js
@@ -13,47 +13,47 @@ const port = process.env.PORT || 3000;
 // Compiles client-side JavaScript code on the fly
 // https://github.com/webpack/webpack-dev-middleware
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack');
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpackConfig = require('./tools/webpack.config').default;
-  app.use(webpackMiddleware(webpack(webpackConfig), { stats: webpackConfig.stats}));
+    const webpack = require('webpack');
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpackConfig = require('./tools/webpack.config').default;
+    app.use(webpackMiddleware(webpack(webpackConfig), { stats: webpackConfig.stats}));
 }
 
 // Register GraphQL middleware
 // https://github.com/graphql/express-graphql
 app.use('/graphql', graphql(req => ({
-  schema,
-  graphiql: true,
-  rootValue: { db: req.app.locals.db }
+    schema,
+    graphiql: true,
+    rootValue: { db: req.app.locals.db }
 })));
 
 // Database access example
 app.get('/test', async (req, res, next) => {
-  try {
-    const db = req.app.locals.db;
-    await db.collection('log').insertOne({
-      time: new Date(),
-      ip: req.ip,
-      message: '/test visit'
-    });
-    res.send('<h1>Hello, world!</h1>');
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const db = req.app.locals.db;
+        await db.collection('log').insertOne({
+            time: new Date(),
+            ip: req.ip,
+            message: '/test visit'
+        });
+        res.send('<h1>Hello, world!</h1>');
+    } catch (err) {
+        next(err);
+    }
 });
 
 // Serve an empty HTML page for all requests (SPA)
 app.get('*', (req, res) => {
-  const markup = ReactDOM.renderToStaticMarkup(<Html />);
-  res.send(`<!doctype html>\n${markup}`);
+    const markup = ReactDOM.renderToStaticMarkup(<Html />);
+    res.send(`<!doctype html>\n${markup}`);
 });
 
 // Create a MonboDB connection pool and start the Node.js app
 MongoClient.connect('mongodb://localhost:27017/demo', { promiseLibrary: Promise })
-  .catch(err => console.error(err.stack))
-  .then(db => {
-    app.locals.db = db; // See http://expressjs.com/en/4x/api.html#app.locals
-    app.listen(port, () => {
-      console.log(`Node.js app is listening at http://localhost:${port}/`);
+    .catch(err => console.error(err.stack))
+    .then(db => {
+        app.locals.db = db; // See http://expressjs.com/en/4x/api.html#app.locals
+        app.listen(port, () => {
+            console.log(`Node.js app is listening at http://localhost:${port}/`);
+        });
     });
-  });
